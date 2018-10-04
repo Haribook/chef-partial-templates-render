39c6b0f3c263fcfce3baa62aa0106e9d8e4ee06e
Hari Krishna Reddy <harikrishna.reddy@synchronoss.com>
2018-10-04 14:37:14 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# render
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: render
+# Recipe:: default
+#
+# Copyright:: 2018, Synchronoss Technologies, Inc., All Rights Reserved.
